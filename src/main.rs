cf32820c36ad32e52dee2d716c5b76abcd3d16ab
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-08-03 19:41:45 -0400

    Build first endpoint of server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+struct ServerState {
+    database_connection: Pool<Postgres>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("")
+        .await
+        .expect("Couldn't establish connection with sql database");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(ServerState {
+                database_connection: pool.clone(),
+            }))
+            .service(post_url)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[post("/urls")]
+async fn post_url() -> impl Responder {
+    HttpResponse::Ok().body("URL Created succesfully")
 }
